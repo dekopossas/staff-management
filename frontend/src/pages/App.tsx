48c286169d00c13703cf5ab5c2e9adb02b0c0446
deko possas <andrepossaster@gmail.com>
2021-06-06 20:00:06 -0300

    page home

--- frontend/src/pages/App.tsx
+++ frontend/src/pages/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "../components/Header";
 import NavSide from "../components/NavSide";
 import '../styles/global.scss'
+import Home from "./Home";
 
 function App() {
   return (
     <div>
       <Header />
       <NavSide />
+      <Home />
     </div>
   );
 }
