fa56142067b901c3dcc5cea945fb216a0396497e
deko possas <andrepossaster@gmail.com>
2021-06-06 19:24:42 -0300

    criado arquivo de side nave

---
+++ frontend/src/components/NavSide/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import styles from './styles.module.scss';
+
+function NavSide() {
+  return (
+    <div className="container">
+      <div className="sidenav" >
+        <div className="nav-list">
+            <i className="material-icons">home</i>
+            Início
+
+            <i className="material-icons">storefront</i>
+            Produtos
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default NavSide;
