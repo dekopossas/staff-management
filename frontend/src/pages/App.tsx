2339ad3fe6c009ef89ceede639e0d404a45d3067
deko possas <andrepossaster@gmail.com>
2021-06-06 19:24:29 -0300

    renderizado o sidenav

--- frontend/src/pages/App.tsx
+++ frontend/src/pages/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../components/Header";
+import NavSide from "../components/NavSide";
 import '../styles/global.scss'
 
 function App() {
   return (
     <div>
       <Header />
+      <NavSide />
     </div>
   );
 }
