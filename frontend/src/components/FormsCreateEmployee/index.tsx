f76331740908f3ec577c027194d0999751b0b2f9
deko possas <andrepossaster@gmail.com>
2021-06-07 17:08:27 -0300

    react-hook-forms funcionando
    retornando um alerta com as informacoes dos campos

--- frontend/src/components/FormsCreateEmployee/index.tsx
+++ frontend/src/components/FormsCreateEmployee/index.tsx
@@ -4,14 +4,14 @@ import styles from './styles.module.scss';
 
 type Profile = {
   fullName: string;
-  cpf: number;
+  cpf: string;
   wage: number;
   discount: string;
   dependents: number;
 };
 
 function FormsCreateEmployee() {
-  const { register, handleSubmit, errors } = useForm<Profile>();
+  const { register, handleSubmit } = useForm<Profile>();
 
   const onSubmit = handleSubmit((data: Profile) => {
     alert(JSON.stringify(data));
@@ -24,57 +24,52 @@ function FormsCreateEmployee() {
         <div className={styles.fullBox}>
           <label htmlFor="fullName">Nome do Funcionário:</label>
           <input
-            ref={register({ required: true })}
+            {...register("fullName")}
             type="text"
             name="fullName"
             id="fullName"
             placeholder="Digite o nome"
           />
-          {errors.fullName && <div className={styles.errorValidation}>Digite o nome</div>}
         </div>
         <div className={styles.halfBox}>
           <label htmlFor="cpf">CPF:</label>
           <input
-            ref={register({ required: true })}
-            type="number"
+            {...register("cpf")}
+            type="text"
             name="cpf"
             id="cpf"
             placeholder="Digite o cpf"
           />
-          {errors.cpf && <div className={styles.errorValidation}>Digite o cpf</div>}
         </div>
         <div className={styles.halfBox}>
           <label htmlFor="wage">Salário Bruto:</label>
           <input
-            ref={register({ required: true })}
+            {...register("wage")}
             type="number"
             name="wage"
             id="wage"
             placeholder="Salário Bruto"
           />
-          {errors.wage && <div className={styles.errorValidation}>Digite o Salário</div>}
         </div>
         <div className={styles.halfBox}>
           <label htmlFor="discount">Desconto na Previdência:</label>
           <input
-            ref={register({ required: true })}
+            {...register("discount")}
             id="discount"
             type="text"
             name="discount"
             placeholder="Desconto na Previdência"
           />
-          {errors.discount && <div className={styles.errorValidation}>Digite o desconto</div>}
         </div>
         <div className={styles.halfBox}>
           <label htmlFor="dependents">Número de Dependentes:</label>
           <input
-            ref={register({ required: true })}
+            {...register("dependents")}
             id="dependents"
             type="number"
             name="dependents"
             placeholder="Número de Dependentes"
           />
-          {errors.dependents && <div className={styles.errorValidation}>Preencha o número</div>}
         </div>
         <div className={styles.halfBox}>
           <input type="submit" value="registrar" id="btn-submit" />
