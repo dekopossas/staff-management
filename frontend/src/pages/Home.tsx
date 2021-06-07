83405132ffffe78a4412977120ff2f5a3379ee09
deko possas <andrepossaster@gmail.com>
2021-06-06 21:22:05 -0300

    hooks com typescript, simplismente fantastico

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
+interface employee {
+    nome: string,
+    cpf: string,
+    salario: number,
+    desconto: number,
+    dependentes: number
+}
 
 function Home() {
+  const [employees, setEmployees] = useState<[employee]>();
+
+  async function loadData() {
+    const response = await fetch('http://localhost:3001/employees');
+    const data = await response.json();
+
+    setEmployees(data);
+  }
+
+  useEffect(() => {
+    loadData();
+  }, [])
+
   return (
     <div>
-      HOME
+      {
+        employees?.map(employee => employee.nome)
+      }
     </div>
   );
 }
+
+
 
 export default Home;
