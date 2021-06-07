92142b0d73daa255d3700e01db19629c008c0814
deko possas <andrepossaster@gmail.com>
2021-06-06 22:52:52 -0300

    create employee feito

---
+++ frontend/src/pages/CreateEmployee/index.tsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from "react";
+import Header from "../../components/Header";
+import NavSide from "../../components/NavSide";
+
+
+function Home() {
+  return (
+    <div>
+      <Header />
+      <NavSide />
+    </div>
+  );
+}
+
+export default Home;
