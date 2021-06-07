d12e1c4919c0b80557286a57f69470690d3794af
deko possas <andrepossaster@gmail.com>
2021-06-07 16:54:58 -0300

    tentando achar o erro

--- frontend/src/components/FormsCreateEmployee/index.tsx
+++ frontend/src/components/FormsCreateEmployee/index.tsx
@@ -8,53 +8,73 @@ type Profile = {
   wage: number;
   discount: string;
   dependents: number;
-}
+};
 
 function FormsCreateEmployee() {
   const { register, handleSubmit, errors } = useForm<Profile>();
 
-  const onSubmit = handleSubmit ((data: Profile) => {
+  const onSubmit = handleSubmit((data: Profile) => {
     alert(JSON.stringify(data));
   });
 
   return (
     <div className={styles.container}>
       <h1>Cadastrando Funcionario</h1>
-      <form onSubmit={onSubmit} className={styles.formRegister}>
+      <form onSubmit={onSubmit} className={styles.form}>
         <div className={styles.fullBox}>
           <label htmlFor="fullName">Nome do Funcionário:</label>
-          <input ref={register({ required: true})} type="text" name="fullName" id="fullName" placeholder="Digite o nome" />
-          {
-            errors.fullName && <div className={styles.errorValidation}>Digite o nome</div>
-          }
+          <input
+            ref={register({ required: true })}
+            type="text"
+            name="fullName"
+            id="fullName"
+            placeholder="Digite o nome"
+          />
+          {errors.fullName && <div className={styles.errorValidation}>Digite o nome</div>}
         </div>
         <div className={styles.halfBox}>
           <label htmlFor="cpf">CPF:</label>
-          <input ref={register({ required: true})} type="number" name="cpf" id="cpf" placeholder="Digite o cpf" />
+          <input
+            ref={register({ required: true })}
+            type="number"
+            name="cpf"
+            id="cpf"
+            placeholder="Digite o cpf"
+          />
+          {errors.cpf && <div className={styles.errorValidation}>Digite o cpf</div>}
         </div>
         <div className={styles.halfBox}>
           <label htmlFor="wage">Salário Bruto:</label>
-          <input ref={register({ required: true})} type="number" name="wage" id="wage" placeholder="Salário Bruto" />
+          <input
+            ref={register({ required: true })}
+            type="number"
+            name="wage"
+            id="wage"
+            placeholder="Salário Bruto"
+          />
+          {errors.wage && <div className={styles.errorValidation}>Digite o Salário</div>}
         </div>
         <div className={styles.halfBox}>
           <label htmlFor="discount">Desconto na Previdência:</label>
           <input
-            ref={register({ required: true})}
+            ref={register({ required: true })}
             id="discount"
             type="text"
             name="discount"
             placeholder="Desconto na Previdência"
           />
+          {errors.discount && <div className={styles.errorValidation}>Digite o desconto</div>}
         </div>
         <div className={styles.halfBox}>
           <label htmlFor="dependents">Número de Dependentes:</label>
           <input
-            ref={register({ required: true})}
+            ref={register({ required: true })}
             id="dependents"
             type="number"
             name="dependents"
             placeholder="Número de Dependentes"
           />
+          {errors.dependents && <div className={styles.errorValidation}>Preencha o número</div>}
         </div>
         <div className={styles.halfBox}>
           <input type="submit" value="registrar" id="btn-submit" />
