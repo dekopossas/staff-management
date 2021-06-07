557b86fffd6ece89b194d208be2eb9097c23c772
deko possas <andrepossaster@gmail.com>
2021-06-07 14:35:28 -0300

    input de nome

--- frontend/src/components/FormsCreateEmployee/index.tsx
+++ frontend/src/components/FormsCreateEmployee/index.tsx
@@ -2,8 +2,18 @@ import { Link } from 'react-router-dom';
 
 function FormsCreateEmployee() {
   return (
-    <div>
-      
+    <div id="main-container">
+      <h1>Cadastrando Funcionario</h1>
+      <form id="register-form">
+        <div className="full-box">
+          <label htmlFor="name" >Nome do Funcionario:
+          
+            <input type="text" name="name" id="name" placeholder="Digite o nome"/>
+          </label>
+
+
+        </div>
+      </form>
     </div>
   );
 }
