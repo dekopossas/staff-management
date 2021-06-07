5992a529869d8bfeabedcef4f32ad402d90a9d47
deko possas <andrepossaster@gmail.com>
2021-06-06 22:07:07 -0300

    browser router cover app

--- frontend/src/pages/App.tsx
+++ frontend/src/pages/App.tsx
@@ -1,16 +1,19 @@
 import React from "react";
+import { BrowserRouter } from 'react-router-dom';
 import Header from "../components/Header";
 import NavSide from "../components/NavSide";
 import '../styles/global.scss'
 import Home from "./Home";
 
+
+
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <Header />
       <NavSide />
       <Home />
-    </div>
+    </BrowserRouter>
   );
 }
 
