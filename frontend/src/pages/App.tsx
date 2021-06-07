6178bfa4bf51da3cae4a658e35476628e32ad8a1
deko possas <andrepossaster@gmail.com>
2021-06-06 22:14:54 -0300

    renderizado direto em rotas

--- frontend/src/pages/App.tsx
+++ frontend/src/pages/App.tsx
@@ -1,18 +1,14 @@
 import React from "react";
 import { BrowserRouter } from 'react-router-dom';
-import Header from "../components/Header";
-import NavSide from "../components/NavSide";
+import Routes from "../routes";
 import '../styles/global.scss'
-import Home from "./Home";
 
 
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
-      <NavSide />
-      <Home />
+      <Routes />
     </BrowserRouter>
   );
 }
