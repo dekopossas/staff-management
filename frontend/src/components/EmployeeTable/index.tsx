ac682a6a2571fe3680aaf450fd688ab2603966f6
deko possas <andrepossaster@gmail.com>
2021-06-08 10:08:21 -0300

    renderizando lista de nomes ja em table

--- frontend/src/components/EmployeeTable/index.tsx
+++ frontend/src/components/EmployeeTable/index.tsx
@@ -1,46 +1,33 @@
+import { useEffect, useState } from 'react';
 import styles from './styles.module.scss';
-import { DataGrid, ColDef, ValueGetterParams } from '@material-ui/data-grid';
 
-const columns: ColDef[] = [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'firstName', headerName: 'First name', width: 130 },
-  { field: 'lastName', headerName: 'Last name', width: 130 },
-  {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 90,
-  },
-  {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (params: ValueGetterParams) =>
-      `${params.getValue('firstName') || ''} ${params.getValue('lastName') || ''}`,
-  },
-];
-
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-]
+interface employee {
+  nome: string,
+  cpf: string,
+  salario: number,
+  desconto: number,
+  dependentes: number
+}
 
 function EmployeeTable() {
+  const [employees, setEmployees] = useState<[employee]>();
+
+  async function loadData() {
+    const response = await fetch('http://localhost:3001/employees');
+    const data = await response.json();
+
+    setEmployees(data);
+  }
+
+  useEffect(() => {
+    loadData();
+  }, [])
+
   return (
     <div className={styles.container}>
-      TABELA
-      <div style={{ height: 400, width: '100%' }}>
-        <DataGrid rows={rows} columns={columns} pageSize={5} checkboxSelection />
-      </div>
+      {
+        employees?.map(employee => <div>{employee.nome}</div>)
+      }
     </div>
   );
 }
