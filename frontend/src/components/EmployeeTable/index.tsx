5f2a69b57c6c424db97b61d433a65e30f9a962cd
deko possas <andrepossaster@gmail.com>
2021-06-08 18:29:21 -0300

    peguei um map pronto de um projeto que eu uso

--- frontend/src/components/EmployeeTable/index.tsx
+++ frontend/src/components/EmployeeTable/index.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import styles from './styles.module.scss';
 
 interface employee {
@@ -28,7 +29,17 @@ function EmployeeTable() {
     <div className={styles.container}>
         <h2>Listagem de Funcionários</h2>
         {
-          employees?.map(employee => <div key={employee.id}>{employee.fullName}</div>)
+          employees?.map((employee, index) => (
+            <Link to={ `/employee/${employee.id}` } key={ employee.id }>
+              <div className="cada-compra">
+                <div data-testid={ `${index}-order-card-container` } className="compra">
+                  <h4 data-testid={ `${index}-order-number` }>{`Pedido ${employee.id}`}</h4>
+                  <h4 data-testid={ `${index}-order-total-value` }>
+                  </h4>
+                </div>
+              </div>
+            </Link>
+          ))
         }
     </div>
   );
