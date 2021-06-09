d94759782744a808e06ae5c2866107eb14142614
deko possas <andrepossaster@gmail.com>
2021-06-09 20:08:28 -0300

    rodando e trabalhando na req de create e edite

--- frontend/src/components/FormsCreateEmployee/index.tsx
+++ frontend/src/components/FormsCreateEmployee/index.tsx
@@ -1,9 +1,8 @@
 import { useHistory, useParams } from 'react-router-dom';
-import { useForm } from 'react-hook-form';
 import styles from './styles.module.scss';
 import api from '../../services/api';
-import { HTTP } from '../../util/constants';
-import { useEffect, useState } from 'react';
+// import { HTTP } from '../../util/constants';
+import { ChangeEvent, useEffect, useState } from 'react';
 
 type Profile = {
   fullName: string;
@@ -14,7 +13,6 @@ type Profile = {
 };
 
 function FormsCreateEmployee() {
-  const { register, handleSubmit } = useForm<Profile>();
   const [employee, setEmployee] = useState<Profile>({
     fullName: '',
     cpf: '',
@@ -22,6 +20,13 @@ function FormsCreateEmployee() {
     discount: '',
     dependents: '',
   });
+
+  function updateEmployee(e: ChangeEvent<HTMLInputElement>) {
+    setEmployee({
+      ...employee,
+      [e.target.name]: e.target.value,
+    });
+  }
 
   const history = useHistory();
   const { id } = useParams<{ id: string }>();
@@ -41,33 +46,6 @@ function FormsCreateEmployee() {
     history.goBack();
   };
 
-  // Preferiria fazer a validação separada com mais calma, esse trecho merece um refactor
-  const onSubmit = handleSubmit(async (data: Profile) => {
-    const { fullName, cpf, wage, dependents, discount } = data;
-    if (fullName !== '' && cpf !== '' && wage !== '' && dependents !== '' && discount !== '') {
-      if (id !== undefined) {
-        const response = await api.post('/employees', data);
-
-        if (response.status === HTTP.CREATED) {
-          history.push('/');
-        } else {
-          alert('Erro ao cadastrar o usuário');
-        }
-      } else {
-        const response = await api.post('/employees', data);
-      
-        if (response.status === HTTP.CREATED) {
-          history.push('/');
-        } else {
-          alert('Erro ao cadastrar o usuário');
-        }
-      }
-
-    } else {
-      alert('Preencha todos os dados, Por favor.');
-    }
-  });
-
   useEffect(() => {
     if (id !== undefined) {
       findEmployee(id);
@@ -77,14 +55,14 @@ function FormsCreateEmployee() {
   return (
     <div className={styles.container}>
       {id ? <h1>Editando Funcionario</h1> : <h1>Cadastrando Funcionario</h1>}
-      <form onSubmit={onSubmit} className={styles.form}>
+      <form className={styles.form}>
         <div className={styles.fullBox}>
           <label htmlFor="fullName">Nome do Funcionário:</label>
           <input
-            {...register('fullName')}
             value={employee.fullName}
             type="text"
             name="fullName"
+            onChange={(event: ChangeEvent<HTMLInputElement>) => updateEmployee(event)}
             id="fullName"
             placeholder="Digite o nome"
           />
@@ -92,9 +70,9 @@ function FormsCreateEmployee() {
         <div className={styles.halfBox}>
           <label htmlFor="cpf">CPF:</label>
           <input
-            {...register('cpf')}
             value={employee.cpf}
             type="number"
+            onChange={(event: ChangeEvent<HTMLInputElement>) => updateEmployee(event)}
             name="cpf"
             id="cpf"
             placeholder="Digite o cpf"
@@ -103,9 +81,9 @@ function FormsCreateEmployee() {
         <div className={styles.halfBox}>
           <label htmlFor="wage">Salário Bruto:</label>
           <input
-            {...register('wage')}
             value={employee.wage}
             type="text"
+            onChange={(event: ChangeEvent<HTMLInputElement>) => updateEmployee(event)}
             name="wage"
             id="wage"
             placeholder="Salário Bruto"
@@ -114,8 +92,8 @@ function FormsCreateEmployee() {
         <div className={styles.halfBox}>
           <label htmlFor="discount">Desconto na Previdência:</label>
           <input
-            {...register('discount')}
             value={employee.discount}
+            onChange={(event: ChangeEvent<HTMLInputElement>) => updateEmployee(event)}
             id="discount"
             type="text"
             name="discount"
@@ -125,8 +103,8 @@ function FormsCreateEmployee() {
         <div className={styles.halfBox}>
           <label htmlFor="dependents">Número de Dependentes:</label>
           <input
-            {...register('dependents')}
             value={employee.dependents}
+            onChange={(event: ChangeEvent<HTMLInputElement>) => updateEmployee(event)}
             id="dependents"
             type="text"
             name="dependents"
@@ -138,15 +116,11 @@ function FormsCreateEmployee() {
             type="submit"
             value={id ? 'Salvar Alterações' : 'Registrar'}
             id="btn-submit"
+            // disabled={!(isEmailValid && isPasswordValid)}
           />
         </div>
         <div className={styles.fullBox}>
-          <input
-            type="button"
-            value="Voltar"
-            id="btn-submit"
-            onClick={backWindow}
-          />
+          <input type="button" value="Voltar" id="btn-submit" onClick={backWindow} />
         </div>
       </form>
     </div>
