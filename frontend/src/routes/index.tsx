161a98c60aa58b2e5e327877b8a7c0351810621a
deko possas <andrepossaster@gmail.com>
2021-06-09 16:51:33 -0300

    vou aproveitar mesmo forms

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,7 +1,5 @@
-import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import CreateEmployee from '../pages/CreateEmployee';
-import EditEmployee from '../pages/EditEmployee';
 import Home from '../pages/Home';
 
 const Routes = () => {
@@ -9,7 +7,7 @@ const Routes = () => {
     <Switch>
       <Route path="/" exact component={ Home } />
       <Route path="/create-employee" exact component={ CreateEmployee } />
-      <Route path="/edit-employee/:id" exact component={ EditEmployee } />
+      <Route path="/edit-employee/:id" exact component={ CreateEmployee } />
     </Switch>
   );
 };
