01644ecc1ad6daa34ae0b72bb00fb76304d592ac
deko possas <andrepossaster@gmail.com>
2021-06-09 15:49:31 -0300

    skeleton of edit employee

---
+++ frontend/src/pages/EditEmployee/index.tsx
@@ -0,0 +1,21 @@
+// import React, { useEffect, useState } from "react";
+import FormsCreateEmployee from "../../components/FormsCreateEmployee";
+import Header from "../../components/Header";
+import NavSide from "../../components/NavSide";
+// import styles from './styles.module.scss';
+import './style.css';
+
+
+function EditEmployee() {
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <NavSide />
+        <FormsCreateEmployee />
+      </div>
+    </>
+  );
+}
+
+export default EditEmployee;
